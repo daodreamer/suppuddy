7dd81fc4f98849871c26d036d714e05a2cc543bf
Jiongdao Wang <jiongdao.wang@gmail.com>
2026-01-25 19:27:38 +0100

    Initial Commit

---
+++ vitamin_calculator/vitamin_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  vitamin_calculatorApp.swift
+//  vitamin_calculator
+//
+//  Created by Jiongdao Wang on 25.01.26.
+//
+
+import SwiftUI
+
+@main
+struct vitamin_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ vitamin_calculatorTests/vitamin_calculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  vitamin_calculatorTests.swift
+//  vitamin_calculatorTests
+//
+//  Created by Jiongdao Wang on 25.01.26.
+//
+
+import Testing
+@testable import vitamin_calculator
+
+struct vitamin_calculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
